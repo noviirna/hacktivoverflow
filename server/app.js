2b6d8c979c6c9316ba6a028f2636a8c836f6d728
noviirna <novi.irnawati@gmail.com>
2019-07-04 02:08:07 +0700

    add kue, node mailer, cron job for question of the week

--- server/app.js
+++ server/app.js
@@ -7,7 +7,19 @@ const express = require("express"),
   cors = require("cors"),
   routes = require("./routes/index"),
   errHandler = require("./middlewares/errHandler"),
-  morgan = require("morgan");
+  morgan = require("morgan"),
+  kue = require("kue"),
+  queue = kue.createQueue(),
+  User = require("./models/user"),
+  Question = require("./models/question"),
+  nodemailer = require("nodemailer"),
+  transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: `${process.env.GOOGLE_EMAIL}`,
+      pass: `${process.env.GOOGLE_PASS}`
+    }
+  });
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -39,8 +51,81 @@ mongoose
 
 app.use("/", routes);
 app.use(errHandler);
+app.use("/kue-ui", kue.app);
 
 app.listen(port, () => {
   console.log("Environment:", process.env.NODE_ENV);
   console.log("Port:", port);
+});
+
+// CRON JOB, KUE / BACKGROUND JOBS, DAN NODE MAILER
+var cron = require("node-cron");
+
+var task = cron.schedule(
+  "0 8 * * 1", //tiap senin jam 8 pagi kirimin question of the week, yaitu question yang paling dapat banyak like di semua periode
+  () => {
+    Question.find({})
+      .populate("userId")
+      .then(questions => {
+        let result = questions.sort(function(a, b) {
+          return b.upvotes.length - a.upvotes.length;
+        });
+        let q_otw = result[0];
+        let emailCont = `
+        this is the question of the week : <br>
+        <hr>
+        <h1>${q_otw.title}</h1><br>
+        <small> by : ${q_otw.userId.username}</small>
+        <p>${q_otw.description}</p>
+        <p>${q_otw.upvotes.length} people upvoted this</p>
+        <p>created at: ${new Date(q_otw.createdAt)}</p>
+        <a href="http://noviirna-overflow.com/question/${
+          q_otw._id
+        }">Checkout this question at our site!</a>
+      `;
+        User.find({})
+          .then(founds => {
+            founds.forEach(user => {
+              const mailOptions = {
+                from: "info@noviirna-overflow.site", // sender address
+                to: `${user.email}`, // list of receivers
+                subject: `Question Of The Week - ${new Date().toISOString}`, // Subject line
+                html: emailCont
+              };
+              transporter
+                .sendMail(mailOptions)
+                .then(sent => {
+                  var qotw_job = queue
+                    .create("qotw", {
+                      title: `QOTW email on monday at 8.00 am sent to ${
+                        user.email
+                      }"`
+                    })
+                    .save(err => {
+                      if (err) {
+                        console.log(err);
+                      }
+                    });
+                })
+                .catch(err => {
+                  console.log(err);
+                });
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log("EMAIL SENT TO ALL USER");
+  },
+  { timezone: "Asia/Jakarta" }
+);
+
+task.start();
+
+queue.process("qotw", (job, done) => {
+  done();
 });
